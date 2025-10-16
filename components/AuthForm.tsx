4880c957bf91ad7aa8e7f2df275dbe6b25cac9a0
CoCoNuTdUmMy <fuoseighadarwin@gmail.com>
2025-10-16 10:16:09 +0100

    first commit setup app, files and folder structure routing and auth pages

---
+++ components/AuthForm.tsx
@@ -0,0 +1,109 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import Image from "next/image"
+
+import { Button } from '@/components/ui/button'
+import{
+  Form,
+} from '@/components/ui/form'
+import Link from "next/link"
+import { toast } from "sonner"
+import FormField from "./FormField"
+import { useRouter } from "next/dist/client/components/navigation"
+
+
+
+interface AuthFormProps{
+  type: 'sign-in' | 'sign-up'
+}
+
+const authFormSchema =(type: FormType) => {
+ return z.object({
+  name: type === 'sign-up' ? z.string().min(3)
+  : z.string().optional(), // cuz we dont need after sign in
+  email: z.string().email(),
+  password: z.string().min(3),
+ })
+}
+
+const AuthForm = ({type}: AuthFormProps) => {
+  const router = useRouter()
+  const formSchema = authFormSchema(type);
+ 
+const form = useForm<z.infer<typeof formSchema>>({
+  resolver: zodResolver(formSchema),
+  defaultValues: {
+    name: "",
+    email: "",
+    password: "",
+
+  },
+})
+ 
+// 2. Define a submit handler.
+function onSubmit(values: z.infer<typeof formSchema>){
+  try{
+    if(type === 'sign-up' ){
+      toast.success("🎉 Account Created Succesfully 🎉  please sign in ");
+       router.push('/sign-in')
+    }else{
+      toast.success(" sign in  successfully");
+       router.push('/')
+
+    }
+  }catch (error) {
+    console.log(error);
+    toast.error(`Oops unExpected error: ${error}`)
+  }
+}
+
+const isSignIn = type === 'sign-in';
+  return (
+    <div className="card-border lg:min-w-[566px]">
+      <div className="flex flex-col gap-6 card 
+      py-14 px-10">
+       <div className="flex flex-row gap-2 justify-center">
+          <h2 className="text-primary-100">PrepSmart</h2>
+          <Image src='/logo.svg' alt='logo' height={32}
+          width={38}/>
+       </div>
+       <h3>Prectice job interview with S.I.G.M.A</h3>
+     <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}
+       className='w-full space-y-6 mt-4 form'>
+        {!isSignIn && (
+          <FormField control={form.control} name='name'
+          label="Name" placeholder='Your Name Here'/>
+        )} 
+
+       <FormField control={form.control} name='email'
+          label="Email" placeholder='Your email address'
+          type="email"/>
+
+          <FormField control={form.control} name='password'
+          label="Password" placeholder='Enter your password'
+          type='password'/>
+      
+      
+       <Button className="btn" type="submit"> {isSignIn ? 'Sign in': 'Create an Account'} </Button>
+      </form>
+     </Form>
+      <p className="text-center">
+        {isSignIn ? 'Dont have an Account?'
+        : 'Have an Account?' }
+        <Link href={!isSignIn ? '/sign-in': '/sign-up' }
+        className="font-bold text-user-primary ml-1">
+          {!isSignIn ? 'Sign in Instead': 'Lets create an Account'}
+        </Link>
+      </p>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default AuthForm
---
+++ constants/index.ts
@@ -0,0 +1,20 @@
+
+
+export const mappings = {
+    'react.js': 'react',
+     reactjs: 'react',
+     react: 'react',
+     'next.js': 'next.js',
+     nextjs: 'nextjs',
+     next: 'nextjs',
+     'vue.js': 'vuejs',
+     vuejs: 'vuejs',
+     vue: 'vuejs',
+     'express.js': 'express',
+     expressjs: 'express',
+     express: 'express',
+     'node.js': 'nodejs',
+     nodejs: 'nodejs',
+     node: 'nodejs',
+     
+}
